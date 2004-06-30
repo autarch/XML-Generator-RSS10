b41ac5b69e739f55cc370fe1bca44dae0b6bccf3
autarch <autarch@de5605a1-f9dd-0310-bdfd-ce6767ad1f48>
2004-06-30 16:01:48 +0000

    reference LICENSE file
    git-svn-id: https://svn.urth.org/svn/XML-Generator-RSS10/trunk@19 de5605a1-f9dd-0310-bdfd-ce6767ad1f48

--- lib/XML/Generator/RSS10.pm
+++ lib/XML/Generator/RSS10.pm
@@ -681,4 +681,7 @@ Copyright 2004 David Rolsky, All Rights Reserved.
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
+The full text of the license can be found in the LICENSE file included
+with this module.
+
 =cut
